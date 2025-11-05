2678c7038b580044bff9c3953cdd1e0151795ee3
SUYEB <suyebmia00@gmail.com>
2025-11-06 04:10:01 +0600

    Fix styling and layout in CategoriesNav component

--- src/components/CatagoriesNav.jsx
+++ src/components/CatagoriesNav.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const CatagoriesNav = ({cetgories, selectCategory, setSelectCategory}) => {
   return (
-    <div className='flex flex-wrap justify-center items-center gap-3 md:gap-5 py-10'>
+    <div className='flex flex-wrap justify-center items-center gap-2 sm:gap-3 md:gap-5 py-6 sm:py-8 md:py-10'>
        {
        cetgories.map(cat=>(
         <button 
         key={cat}
         onClick={()=> setSelectCategory(cat)}
-        className={`px-4 py-2 sm:px-2 sm:py-1 rounded-full font-medium transition-all duration-300
+        className={`px-2 py-1 text-sm
+            sm:px-3 sm:py-1.5 sm:text-base 
+            md:px-4 md:py-2 md:text-lg rounded-full font-medium transition-all duration-300
            ${selectCategory === cat ? "bg-[#ad191b] text-white" : "border border-red-500 text-[#ad191b] hover:bg-[#ad191b] hover:text-white"} 
             
             `}
