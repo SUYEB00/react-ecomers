ab556a65b2219af36845d204ba9d6cdc2889cba7
suyeb00 <suyeb00@gmail.com>
2026-06-02 23:46:03 +0600

    Update firbase

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3GPSSN1PlBqt-1zjOrmMUCFWp3Q5PfSE",
-  authDomain: "fir-63d48.firebaseapp.com",
-  projectId: "fir-63d48",
-  storageBucket: "fir-63d48.firebasestorage.app",
-  messagingSenderId: "827205188404",
-  appId: "1:827205188404:web:eae61ea0e2abb9a1064361",
-  measurementId: "G-NJE9HC8YYR"
+  apiKey: "AIzaSyDDYfLJ4ZXmja3QD0urdQkFRfipES6yvfY",
+  authDomain: "trendzonelive-8ad9d.firebaseapp.com",
+  projectId: "trendzonelive-8ad9d",
+  storageBucket: "trendzonelive-8ad9d.firebasestorage.app",
+  messagingSenderId: "615872759803",
+  appId: "1:615872759803:web:a37de39ef03599c7ba6d5e",
+  measurementId: "G-77LWWKZ3L1"
 };
 
 const app = initializeApp(firebaseConfig);
