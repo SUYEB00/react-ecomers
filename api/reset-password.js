33431291cd3cddd29524e64e018fd359accb1921
suyeb00 <suyeb00@gmail.com>
2026-06-04 01:56:11 +0600

    use gmail sender

--- api/reset-password.js
+++ api/reset-password.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     });
 
     await transporter.sendMail({
-      from: "TrendZone <support@trendzone.live>",
+      from: "TrendZone <trendzone033@gmail.com>",
       to: email,
       subject: "Reset Your TrendZone Password",
       html: `
